36f9e02e0685af9a2a1b1a32be34acfc6eebaa79
Alexander Ryzhikov <coobaha@gmail.com>
2021-01-29 19:34:21 +0200

    Fix tsd

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,12 @@
 import { AwilixContainer } from 'awilix'
 import fastify, { FastifyInstance } from "fastify";
-import { diContainer, FastifyAwilixOptions, fastifyAwilixPlugin } from './'
+import { diContainer, FastifyAwilixOptions, fastifyAwilixPlugin, Cradle } from './'
 import { expectAssignable, expectType } from 'tsd'
 
 expectAssignable<FastifyAwilixOptions>({})
 expectAssignable<FastifyAwilixOptions>({ disposeOnClose: false})
 expectAssignable<FastifyAwilixOptions>({ disposeOnResponse: false})
-expectType<AwilixContainer>(diContainer)
+expectType<AwilixContainer<Cradle>>(diContainer)
 
 const app: FastifyInstance = fastify();
 app.register(fastifyAwilixPlugin, {});
