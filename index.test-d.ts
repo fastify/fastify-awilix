2cd471b6daed9a78f617957a70fc9a80c0baa9bc
Alexander Ryzhikov <coobaha@gmail.com>
2021-01-30 03:56:55 +0200

    Add interface merging example and assertions to tsd file

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,53 @@
-import { AwilixContainer } from 'awilix'
-import fastify, { FastifyInstance } from "fastify";
-import { diContainer, FastifyAwilixOptions, fastifyAwilixPlugin, Cradle } from './'
+import { asValue, AwilixContainer } from 'awilix'
+import fastify, { FastifyInstance } from 'fastify'
+import { diContainer, FastifyAwilixOptions, fastifyAwilixPlugin, Cradle, RequestCradle } from './'
 import { expectAssignable, expectType } from 'tsd'
 
 expectAssignable<FastifyAwilixOptions>({})
-expectAssignable<FastifyAwilixOptions>({ disposeOnClose: false})
-expectAssignable<FastifyAwilixOptions>({ disposeOnResponse: false})
-expectType<AwilixContainer<Cradle>>(diContainer)
+expectAssignable<FastifyAwilixOptions>({ disposeOnClose: false })
+expectAssignable<FastifyAwilixOptions>({ disposeOnResponse: false })
 
-const app: FastifyInstance = fastify();
-app.register(fastifyAwilixPlugin, {});
+interface MailService {
+  greet(name: string): void
+}
+interface User {
+  name: string
+}
+
+declare module './' {
+  interface Cradle {
+    mailService: MailService
+  }
+  interface RequestCradle {
+    user: User
+  }
+}
+
+expectType<AwilixContainer<Cradle>>(diContainer)
+expectType<MailService>(diContainer.cradle.mailService)
+expectType<MailService>(diContainer.resolve('mailService'))
+
+const app: FastifyInstance = fastify()
+
+app.register(fastifyAwilixPlugin, {})
+
+app.addHook('onRequest', (request, reply, done) => {
+  request.diScope.register({
+    user: asValue({
+      name: 'John Doe',
+    }),
+  })
+  done()
+})
+
+app.get('/user', (request) => {
+  expectType<AwilixContainer<Cradle & RequestCradle>>(request.diScope)
+
+  const mailService = request.diScope.cradle.mailService
+  const user = request.diScope.cradle.user
+
+  expectType<MailService>(mailService)
+  expectType<User>(user)
+
+  mailService.greet(user.name)
+})
