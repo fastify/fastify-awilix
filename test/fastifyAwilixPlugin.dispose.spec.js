c46a2bacc44baaf5383b6831ae57747d77e30480
Frazer Smith <frazer.dev@outlook.com>
2023-10-18 16:55:45 +0100

    chore: add missing `use strict` directives (#117)

--- test/fastifyAwilixPlugin.dispose.spec.js
+++ test/fastifyAwilixPlugin.dispose.spec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const fastify = require('fastify')
 const { asClass, Lifetime } = require('awilix')
 const { fastifyAwilixPlugin, diContainer } = require('../lib')
--- test/fastifyAwilixPlugin.spec.js
+++ test/fastifyAwilixPlugin.spec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const fastify = require('fastify')
 const { asValue, asFunction, asClass, Lifetime } = require('awilix')
 const { fastifyAwilixPlugin, diContainer } = require('../lib')
